0d4e7f390fb676daeeb98f329dbf4cee5b1a5c1b
Mark J Perryman <tazmancoder@gmail.com>
2024-06-28 18:04:56 -0600

    Initial Commit

---
+++ MySwiftCal/MySwiftCalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftCalApp.swift
+//  MySwiftCal
+//
+//  Created by Mark Perryman on 6/28/24.
+//
+
+import SwiftUI
+
+@main
+struct MySwiftCalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
